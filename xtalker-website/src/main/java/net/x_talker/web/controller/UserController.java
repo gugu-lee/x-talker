cf6fbc98d83652baba605568f7469eff6ed17241
gugu-lee <seven.stone.2012@gmail.com>
2015-02-22 01:11:04 +0800

    add REST servie,not complete

--- xtalker-website/src/main/java/net/x_talker/web/controller/UserController.java
+++ xtalker-website/src/main/java/net/x_talker/web/controller/UserController.java
@@ -1,15 +1,12 @@
 package net.x_talker.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import net.x_talker.web.dao.*;
-import net.x_talker.web.entity.IMPI;
-import net.x_talker.web.entity.IMSU;
-import net.x_talker.web.entity.IMPU;
-import net.x_talker.web.entity.IMPI_IMPU;
-import net.x_talker.web.entity.IMPU_VisitedNetwork;
-import net.x_talker.web.entity.UserConf;
+import net.x_talker.web.entity.ActionResult;
 
 @RestController
 public class UserController {
@@ -17,21 +14,66 @@ public class UserController {
 	private IUserDao userDao; 
 	
 	@RequestMapping(value = "/service/user/register", method = RequestMethod.POST)
-	public  UserConf create()
+	public  ActionResult register(@RequestParam("userName") String userName,
+			@RequestParam("email") String email,
+			@RequestParam("inputPassword") String password,
+			@RequestParam("reInputPassword") String rePassword,
+			HttpServletRequest request)
 	{
-		
-		return null;
+		UserService service = new UserService();
+		ActionResult result = new ActionResult();
+		if (!password.equals(rePassword)){
+			result.setErrorMessage("password and re-password is not same.");
+			result.setErrorNo(4);
+			request.setAttribute("result", result);
+			return result;
+		}
+		result = service.register(userDao, userName+"@x-talker.net", password, email);
+		if (result.getErrorNo()!=0){
+			request.setAttribute("result", result);
+			return result;
+		}
+		return result;
 	}
 	
-	@RequestMapping(value = "/user/fetch", method = RequestMethod.GET)  
-	public IMPI fetchIMPI(int id)
+	@RequestMapping(value = "/service/user/fetch", method = RequestMethod.GET)  
+	public ActionResult fetchIMPI(@RequestParam("identity") String identity)
 	{
-		return null;
+		UserService service = new UserService();
+		ActionResult result = service.fetch(userDao, identity);
+		return result;
 	}
 	
-	@RequestMapping(value = "/user/resetPassword", method = RequestMethod.GET)
-	public boolean resetPassword(String oldPassword,String newPassword)
+	@RequestMapping(value = "/service/user/login", method = RequestMethod.POST)
+	public ActionResult login(@RequestParam("identity") String identity,@RequestParam("k") String password)
 	{
-		return true;
+
+		UserService service = new UserService();
+		identity += "@x-talker.net";
+		ActionResult result = service.login(userDao,identity, password);
+		/*
+		if (result.getErrorNo()==0){
+			return result;
+		}*/
+		return result;
+	}
+	
+	@RequestMapping(value = "/service/user/resetPassword", method = RequestMethod.GET)
+	public ActionResult resetPassword(@RequestParam("identity") String identity,@RequestParam("inputPassword") String password,
+			@RequestParam("reInputPassword") String rePassword)
+	{
+		UserService service = new UserService();
+		ActionResult result = new ActionResult();
+		if (!password.equals(rePassword)){
+			result.setErrorMessage("password and re-password is not same.");
+			result.setErrorNo(4);
+			return result;
+		}
+		result = service.resetPassword(userDao, identity, password);
+		//if (result.getErrorNo()!=0){
+
+		//	return result;
+		//}
+		return result;
 	}
 }
